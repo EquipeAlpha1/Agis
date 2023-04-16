dd8ef7ed74c4ae7370b5c8f868785c8b66274bd7
Pedro Davi <pedro.henrique04896@gmail.com>
2023-04-16 17:17:56 -0300

    inicio interface cadastro de escola

--- ProjetoAgis/src/org/equipealpha/agis/view/CadastroEscola.java
+++ ProjetoAgis/src/org/equipealpha/agis/view/CadastroEscola.java
@@ -8,11 +8,26 @@ package org.equipealpha.agis.view;
  *
  * @author Pedro Davi
  */
+import javax.swing.*;
+import java.awt.*;
+
 public class CadastroEscola extends InterfaceBase{
+    
+    private JLabel txtCadastro;
+    CadastroEscola(){super();}
 
     @Override
-    protected void addConteudo() {
-        
+    protected void addconteudo() {
+
+        txtCadastro = new JLabel();
+        txtCadastro.setText("Cadastro de Escola");
+        txtCadastro.setFont(new Font("Times new Roman", Font.BOLD, 25));
+        txtCadastro.setHorizontalAlignment(450);
+        txtCadastro.setVerticalAlignment(150);
+        txtCadastro.setVisible(true);
+
+
+        conteudo.add(txtCadastro);
+
     }
-    
 }
