f63bf4d198c24228c9caba73c72282b5fe1c2434
NatanaelSM <natanaelsm0102@gmail.com>
2023-05-08 09:40:24 -0300

    update

---
+++ ProjetoAgis/src/org/equipealpha/agis/controller/GerenciamentoEscolar.java
@@ -0,0 +1,28 @@
+package org.equipealpha.agis.controller;
+
+import org.equipealpha.agis.model.Aluno;
+import java.util.ArrayList;
+import org.equipealpha.agis.model.Escola;
+import org.equipealpha.agis.model.Prova;
+import org.equipealpha.agis.model.Tarefa;
+import org.equipealpha.agis.model.Trabalho;
+import org.equipealpha.agis.model.Turma;
+
+public class GerenciamentoEscolar {
+
+    private ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+    private ArrayList<Turma> turmas = new ArrayList<Turma>();
+    private ArrayList<Escola> escolas = new ArrayList<Escola>();
+    private ArrayList<Trabalho> trabalhos = new ArrayList<Trabalho>();
+    private ArrayList<Tarefa> tarefas = new ArrayList<Tarefa>();
+    private ArrayList<Prova> provas = new ArrayList<Prova>();
+
+    public void estatisticasTurma(){
+        
+    }
+
+    public void estatiticaAluno(){
+
+    }
+
+}
